3e813884b9ad90612dbcdb45aa66060500125936
Arnav R <you@example.com>
2020-06-30 23:59:24 +0530

    new

---
+++ app/src/main/java/com/arnav/TextInputLayout/FabAndSnackbarActivity.java
@@ -0,0 +1,12 @@
+package com.arnav.TextInputLayout;
+
+import android.app.Activity;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+
+public class FabAndSnackbarActivity extends Activity {
+
+
+
+}
